31d0466328ebaa26fa0edc382ba00156dcb94229
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-11-16 23:50:57 +0100

    Add files via upload

---
+++ tomcMatijaPonavlajnjeZd7/tomcMatijaPonavlajnjeZd7/GeometriskoTijelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPonavlajnjeZd7
+{
+    class GeometriskoTijelo
+    {
+        double volumen;
+        double oplosje;
+        public static int brojacTijela;
+
+        public double Volumen { get => volumen; set => volumen = value; }
+        public double Oplosje { get => oplosje; set => oplosje = value; }
+
+        public GeometriskoTijelo()
+        {
+            brojacTijela++;
+        }
+    }
+}
